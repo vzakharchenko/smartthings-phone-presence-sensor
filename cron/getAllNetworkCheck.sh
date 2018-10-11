6902da43b01c3f6f63e6e0a9bbb02ecef85e2202
vzakharchenko <vzakharchenko@provectus.com>
2018-10-11 21:51:47 +0300

    updated cron

--- cron/getAllNetworkCheck.sh
+++ cron/getAllNetworkCheck.sh
@@ -1,3 +1,3 @@
-croncmd="http://192.100.200.122:5000/getAllNetwork 2>&1"
+croncmd="http://localhost:5000/getAllNetwork 2>&1"
 cronjob="*/3 * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
