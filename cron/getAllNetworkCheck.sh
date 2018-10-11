3c904ae3cb053e61f42c3a41665b51477336eb4c
vzakharchenko <vzakharchenko@provectus.com>
2018-10-11 22:15:05 +0300

    fix presence API

--- cron/getAllNetworkCheck.sh
+++ cron/getAllNetworkCheck.sh
@@ -1,3 +1,3 @@
-croncmd="http://localhost:5000/getAllNetwork 2>&1"
+croncmd="curl http://localhost:5000/getAllNetwork 2>&1"
 cronjob="*/3 * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
