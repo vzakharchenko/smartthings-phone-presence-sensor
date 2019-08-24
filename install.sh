17660c8385d4f40222089d71fedb832e7722ff00
vzakharchenko <vzakharchenko@provectus.com>
2019-08-24 10:38:27 +0300

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ currentUser=`whoami`
 echo ${currentUser}
 sudo apt-get update
 sudo apt-get install curl
-sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs git
 sudo npm i pm2 -g
