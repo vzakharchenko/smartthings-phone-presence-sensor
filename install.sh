d69b952ec039795d8ffbd05ef1d782667904b27e
Vasiliy <vaszakharchenko@gmail.com>
2020-02-05 21:31:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs git npm
 sudo npm i pm2 -g
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ${currentUser} --hp ${HOME}
+sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ${currentUser} --hp ${HOME}
 sudo mkdir -p /opt/app
 sudo rm -rf /opt/app/router
 sudo mkdir -p /opt/config
