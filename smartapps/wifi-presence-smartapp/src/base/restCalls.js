5d274550d2283fdc1644f4762584253464ed28f2
vzakharchenko <vzakharchenko@modeln.com>
2021-05-30 11:18:48 +0300

    fixed lint error

---
+++ smartapps/wifi-presence-smartapp/src/base/restCalls.js
@@ -0,0 +1,30 @@
+const fetch = require('axios');
+
+async function fetchData(url, method = 'GET', headers) {
+  const ret = await fetch({
+    url,
+    method,
+    headers,
+    transformResponse: (req) => req,
+    withCredentials: true,
+    timeout: 4900,
+  });
+  return ret.data;
+}
+
+async function sendData(url, method = 'POST', data, headers) {
+  const ret = await fetch({
+    url,
+    method,
+    data,
+    transformResponse: (req) => req,
+    headers,
+    withCredentials: true,
+    timeout: 4900,
+  });
+  return ret.data;
+}
+module.exports = {
+  fetchData,
+  sendData,
+};
