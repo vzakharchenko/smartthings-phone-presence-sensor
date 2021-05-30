b451a4dc1090b76dad8f39f613c7c9d7940de256
vzakharchenko <vzakharchenko@modeln.com>
2021-05-30 11:20:33 +0300

    fixed lint error

--- RouterSmartAppServer.js
+++ RouterSmartAppServer.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const env = require('./lib/env.js');
+const env = require('./lib/env');
 const {
   getUsers,
   assignMacToUser,
